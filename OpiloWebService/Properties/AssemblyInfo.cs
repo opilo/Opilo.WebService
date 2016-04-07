5d248cb3108a76c065537e19e1e5abb7a62eecd5
SajadRobati <ss.robati@gmail.com>
2016-04-07 21:43:21 +0430

    Update AssemblyInfo

--- OpiloWebService/Properties/AssemblyInfo.cs
+++ OpiloWebService/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OpiloWebService")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Opilo WebService Client")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Opilo")]
 [assembly: AssemblyProduct("OpiloWebService")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © Opilo 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
