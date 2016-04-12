328b8a7b918975d570014888a3be9ad51460ea47
SajadRobati <ss.robati@gmail.com>
2016-04-12 15:48:55 +0430

    add integration tests

---
+++ OpiloWebService.Test/Integration/OpiloClientBasicTest.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using OpiloWebService.Configs;
+using OpiloWebService.Request;
+using OpiloWebService.Response;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace OpiloWebService.Test.Integration
+{
+    [TestFixture]
+    class OpiloClientBasicTest
+    {
+        private string OPILO_URL = ConfigurationSettings.AppSettings["OPILO_URL"];
+        private string OPILO_USERNAME = ConfigurationSettings.AppSettings["OPILO_USERNAME"];
+        private string OPILO_PASSWORD = ConfigurationSettings.AppSettings["OPILO_PASSWORD"];
+        private string PANEL_LINE = ConfigurationSettings.AppSettings["PANEL_LINE"];
+        private string DESTINATION = ConfigurationSettings.AppSettings["DESTINATION"];
+
+        private OpiloWebService.V2.OpiloClient client;
+
+        [SetUp]
+        public void setUp()
+        {
+            ConnectionConfig config = new ConnectionConfig(OPILO_URL);
+            Account account = new Account(OPILO_USERNAME, OPILO_PASSWORD);
+            this.client = new V2.OpiloClient(config, account);
+        }
+
+        [Test]
+        public void testGetCredit()
+        {
+            Credit credit = this.client.getCredit();
+            Assert.True(credit.SmsPageCount >= 0);
+        }
+
+        [Test]
+        public void testSendSingleSMS()
+        {
+            int initCredit = this.client.getCredit().SmsPageCount;
+            List<OutgoingSMS> messages = new List<OutgoingSMS>();
+            messages.Add(new OutgoingSMS(PANEL_LINE, DESTINATION, "V2::testSendSingleSMS()"));
+            List<SendSMSResponse> response = client.sendSMS(messages);
+            Assert.AreEqual(1, response.Count);
+            Assert.IsInstanceOf<SMSId>(response[0]);
+            CheckStatusResponse status = this.client.checkStatus(new List<int>() { int.Parse(((SMSId)response[0]).Id) });
+            Assert.AreEqual(1, status.StatusArray.Count);
+            int finalCredit = this.client.getCredit().SmsPageCount;
+            Assert.LessOrEqual(initCredit - finalCredit, 1);
+        }
+
+        [Test]
+        public void testSendMultipleSMS()
+        {
+            int initCredit = this.client.getCredit().SmsPageCount;
+            List<OutgoingSMS> messages = new List<OutgoingSMS>();
+            for (int i = 0; i < 10; i++)
+                messages.Add(new OutgoingSMS(PANEL_LINE, DESTINATION, "V2::testSendMultipleSMS" + string.Format("({0})", i), i.ToString(), DateTime.Now.AddMinutes(i)));
+
+            List<SendSMSResponse> response = client.sendSMS(messages);
+            Assert.AreEqual(10, response.Count);
+            List<int> ids = new List<int>();
+            foreach (var id in response)
+            {
+                Assert.IsInstanceOf<SMSId>(id);
+                ids.Add(int.Parse(((SMSId)id).Id));
+            }
+            CheckStatusResponse status = this.client.checkStatus(ids);
+            Assert.AreEqual(10, status.StatusArray.Count);
+            int finalCredit = this.client.getCredit().SmsPageCount;
+            Assert.LessOrEqual(initCredit - finalCredit, 10);
+        }
+
+        [Test]
+        public void testCheckInbox()
+        {
+            Inbox inbox = this.client.checkInbox(0);
+            List<IncomingSMS> messages = inbox.Messages;
+            Assert.LessOrEqual(messages.Count, Inbox.PAGE_LIMIT);
+        }
+    }
+}
---
+++ OpiloWebService.Test/Integration/OpiloClientExceptionTest.cs
@@ -0,0 +1,122 @@
+﻿using NUnit.Framework;
+using OpiloWebService.Configs;
+using OpiloWebService.Request;
+using OpiloWebService.Response;
+using OpiloWebService.V2;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace OpiloWebService.Test.Integration
+{
+    [TestFixture]
+    class OpiloClientExceptionTest
+    {
+        private string OPILO_URL = ConfigurationSettings.AppSettings["OPILO_URL"];
+        private string OPILO_USERNAME = ConfigurationSettings.AppSettings["OPILO_USERNAME"];
+        private string OPILO_PASSWORD = ConfigurationSettings.AppSettings["OPILO_PASSWORD"];
+        private string PANEL_LINE = ConfigurationSettings.AppSettings["PANEL_LINE"];
+        private string DESTINATION = ConfigurationSettings.AppSettings["DESTINATION"];
+        private string OPILO_USERNAME_WS_DISABLED = ConfigurationSettings.AppSettings["OPILO_USERNAME_WS_DISABLED"];
+        private string OPILO_PASSWORD_WS_DISABLED = ConfigurationSettings.AppSettings["OPILO_PASSWORD_WS_DISABLED"];
+
+        [Test]
+        public void test401()
+        {
+            ConnectionConfig config = new ConnectionConfig(OPILO_URL);
+            Account account = new Account(OPILO_USERNAME, "wrong_password");
+            OpiloClient client = new OpiloClient(config, account);
+            try
+            {
+                client.getCredit();
+            }
+            catch (CommunicationException ex)
+            {
+                Assert.AreEqual("Authentication Failed", ex.Message);
+                Assert.AreEqual(CommunicationException.AUTH_ERROR, ex.Code);
+            }
+        }
+
+        [Test]
+        public void test403()
+        {
+            ConnectionConfig config = new ConnectionConfig(OPILO_URL);
+            Account account = new Account(OPILO_USERNAME_WS_DISABLED, OPILO_PASSWORD_WS_DISABLED);
+            OpiloClient client = new OpiloClient(config, account);
+            try
+            {
+                client.getCredit();
+            }
+            catch (CommunicationException ex)
+            {
+                Assert.AreEqual("Forbidden [Web-service is disabled]", ex.Message);
+                Assert.AreEqual(CommunicationException.FORBIDDEN, ex.Code);
+            }
+        }
+
+        [Test]
+        public void test422()
+        {
+            ConnectionConfig config = new ConnectionConfig(OPILO_URL);
+            Account account = new Account(OPILO_USERNAME, OPILO_PASSWORD);
+            OpiloClient client = new OpiloClient(config, account);
+            bool failed = false;
+            try
+            {
+                client.checkStatus(new List<int>() { -1 });
+            }
+            catch (CommunicationException ex)
+            {
+                failed = true;
+                Assert.AreEqual("Input Validation Failed", ex.Message);
+                Assert.AreEqual(CommunicationException.INVALID_INPUT, ex.Code);
+            }
+            Assert.IsTrue(failed);
+        }
+
+        [Test]
+        public void test422Errors()
+        {
+            ConnectionConfig config = new ConnectionConfig(OPILO_URL);
+            Account account = new Account(OPILO_USERNAME, OPILO_PASSWORD);
+            OpiloClient client = new OpiloClient(config, account);
+            bool failed = false;
+            try
+            {
+                client.checkStatus(new List<int>() { 0 });
+            }
+            catch (ValidationException ex)
+            {
+                failed = true;
+                List<Dictionary<string, string>> errors = ex.Errors;
+                Assert.AreEqual(1, errors.Count);
+                Assert.AreEqual("1", errors[0]["Min"]);
+            }
+            Assert.IsTrue(failed);
+        }
+
+        [Test]
+        public void testSendInvalidSMS()
+        {
+            ConnectionConfig config = new ConnectionConfig(OPILO_URL);
+            Account account = new Account(OPILO_USERNAME, OPILO_PASSWORD);
+            OpiloClient client = new OpiloClient(config, account);
+            List<OutgoingSMS> messages = new List<OutgoingSMS>();
+            messages.Add(new OutgoingSMS("abcd", DESTINATION, "invalid from"));
+            messages.Add(new OutgoingSMS(PANEL_LINE, "abcd", "invalid to"));
+            messages.Add(new OutgoingSMS("3000", DESTINATION, "unauthorized from"));
+            messages.Add(new OutgoingSMS(PANEL_LINE, DESTINATION, "authorized from"));
+
+            List<SendSMSResponse> response = client.sendSMS(messages);
+            Assert.AreEqual(4, response.Count);
+            Assert.IsInstanceOf<SendError>(response[0]);
+            Assert.IsInstanceOf<SendError>(response[1]);
+            Assert.IsInstanceOf<SendError>(response[2]);
+            Assert.IsInstanceOf<SMSId>(response[3]);
+            Assert.AreEqual(SendError.ERROR_RESOURCE_NOT_FOUND, ((SendError)response[0]).Error);
+            Assert.AreEqual(SendError.ERROR_INVALID_DESTINATION, ((SendError)response[1]).Error);
+            Assert.AreEqual(SendError.ERROR_RESOURCE_NOT_FOUND, ((SendError)response[2]).Error);
+        }
+    }
+}
