55c5cdb7c155e91e7e59a13fae021226698b4023
SajadRobati <ss.robati@gmail.com>
2016-04-12 15:47:06 +0430

    bugfix ValidationException.extractErrors

--- OpiloWebService/Response/ValidationException.cs
+++ OpiloWebService/Response/ValidationException.cs
@@ -9,7 +9,7 @@ namespace OpiloWebService.Response
     {
         private string httpResponseBody = "";
 
-        private List<string> errors = new List<string>();
+        private List<Dictionary<string, string>> errors = new List<Dictionary<string, string>>();
 
         public ValidationException(int code, string httpResponseBody)
             : base(code)
@@ -36,12 +36,19 @@ namespace OpiloWebService.Response
         {
             try
             {
-                JArray jArray = JArray.Parse(this.httpResponseBody);
-                if( jArray["errors"] != null )
+                JObject jResponse = JObject.Parse(this.httpResponseBody);
+                if (jResponse["errors"] != null)
                 {
-                    JArray jErrors = (JArray)jArray["errors"];
-                    for (int i = 0; i < jErrors.Count; i++)
-                        this.errors.Add(jErrors[i].ToString());
+                    JObject jErrors = (JObject)jResponse["errors"];
+                    for (int i = 0; i < jErrors.Count; i++) {
+                        JObject jError = (JObject)jErrors["ids." + i.ToString()];
+                        Dictionary<string,string> dic = new Dictionary<string,string>();
+                        if (jError["Integer"] != null)
+                            dic.Add("Integer", "");
+                        if (jError["Min"] != null)
+                            dic.Add("Min", ((JArray)jError["Min"])[0].ToString());
+                        errors.Add(dic);
+                    }
                 }
             }
             catch (Exception ex) { }
@@ -56,7 +63,7 @@ namespace OpiloWebService.Response
             }
         }
 
-        public List<string> Errors
+        public List<Dictionary<string, string>> Errors
         {
             get
             {
